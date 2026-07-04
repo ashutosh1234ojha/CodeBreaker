51ec8cd6e919fb78598b549418dfce548a80be1a
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2026-07-04 22:21:13 +1000

    Code reformat

---
+++ DummyProjectiOS/PegView.swift
@@ -0,0 +1,18 @@
+//
+//  PegView.swift
+//  DummyProjectiOS
+//
+//  Created by ashutosh ojha on 04/07/26.
+//
+
+import SwiftUI
+
+struct PegView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PegView()
+}
