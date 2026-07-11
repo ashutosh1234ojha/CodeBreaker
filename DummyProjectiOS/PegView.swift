85ce63db18c863927a3793ed5839f129ab12e31a
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2026-07-11 21:45:55 +1000

    Feat: overlay and peg shape changed

--- DummyProjectiOS/PegView.swift
+++ DummyProjectiOS/PegView.swift
@@ -13,16 +13,9 @@ struct PegView: View {
     
     //MARK: -Body
   
-    let pegShape = RoundedRectangle(cornerRadius:10)
+    let pegShape = Circle()
     var body: some View {
         pegShape
-                .overlay{
-                    if(peg == Code.missingPeg){
-                        pegShape
-                            .strokeBorder(Color.gray)
-        
-                    }
-                }
             .contentShape(pegShape)
             .aspectRatio(1, contentMode: .fit)
             .foregroundStyle(peg)
